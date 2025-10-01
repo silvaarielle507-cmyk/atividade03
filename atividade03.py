d990f265a6e9e6eb5b8aa7a54b5d316852b77ff9
silvaarielle507-cmyk <silvaarielle507@gmil.com.br>
2025-10-01 12:51:03 -0300

    first commit

---
+++ atividade03.py
@@ -0,0 +1,8 @@
+numero_inteiro= int(input("digite seu numero: "))
+numero_decimal= float(input("digite o seu numero: "))
+texto= str(input("digite o texto: "))
+valor_logico= bool(input("digite o valor: "))
+
+
+
+    
